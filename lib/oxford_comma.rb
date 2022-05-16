f258d195835213b2671e06e7e3003f5d0c2058e8
winifredf <winifred.frierson@gmail.com>
2022-05-16 13:46:21 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,21 +1,9 @@
-# def oxford_comma(array)
-#   if  array.size == 1
-#     return array[0]
-#   elsif array.size == 2
-#     return array.join(" and ")
-#   else 
-#     return array[0..-2].join(', ') + ", and " + array[-1]
-#   end
-# end
-
-
 def oxford_comma(array)
   if array.size == 1
     return "kiwi"
   elsif array.size == 2
     return array.join(" and ")
   elsif array.size == 3
-    # return array.join " , and " + array[-1]
     return array[0..-2].join(', ') + ", and " + array[-1]
   else 
     return array[0..-2].join(", ") + (", and " + array[-1])
